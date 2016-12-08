e0122ae8ee972cc8af2cc3aa539ae1a47f05af15
Mikolaj Stachura <stachuram@gmail.com>
2016-12-08 01:40:05 +0100

    Change of flow - process each record as it appears, write everything at the end

--- src/uk/org/physiofirst/PhysioScrap.java
+++ src/uk/org/physiofirst/PhysioScrap.java
@@ -12,9 +12,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
@@ -26,7 +24,6 @@ public class PhysioScrap {
 		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
 		FirefoxProfile firefoxProfile = new FirefoxProfile();
 		firefoxProfile.setPreference("permissions.default.image", 2);
-		
 		//FF
 		FirefoxDriver driver = new FirefoxDriver(ffBinary, firefoxProfile);
 		
@@ -40,7 +37,6 @@ public class PhysioScrap {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date = new Date();
 		System.out.println("Start at: " + dateFormat.format(date));
-
 		Workbook wb = null;
 		try {
 			wb = WorkbookFactory.create(new File("out.xlsx"));
@@ -61,24 +57,81 @@ public class PhysioScrap {
 		By byPracticeName = By.className("blue-grey-light");
 
 		int rownum = 0;
-
 		long start_time = 0;
 		long end_time = 0;
 		long difference = 0;
-		//int startpos = 0;
-		//int endpos = 9;
+		long for_start_time = 0;
+		long for_end_time = 0;
+		long for_difference = 0;
+		int startpos = 1;
+		int endpos = 10;
+		int refno = 40000;
 		String profession = "Physiotherapist";
 		
 		while (true) {
-			 
-			//List<WebElement> paczka = driver.findElements(byListItem);
-			
 			start_time = System.currentTimeMillis();
+			int size = driver.findElements(byListItem).size();
+			if( endpos > 4000 && endpos > size)
+				endpos = size;
+			 
+			for (int it = startpos; it <= endpos; it++) {
+				for_start_time = System.currentTimeMillis();
+				String byListItemText = ".//*[@class='articles-item']" + "[" + Integer.toString(it)  + "]";
+				WebElement we = driver.findElement(By.xpath(byListItemText));
+				start_time = System.currentTimeMillis();
+				rownum++;
+				sh.createRow(rownum);
+				Row r1 = sh.getRow(rownum);
+				List<WebElement> practiceName = we.findElements(byPracticeName);
+				List<WebElement> nameAddress = we.findElements(byNameAddressText);
+				List<WebElement> emailSection = we.findElements(byEmailUs);
+				List<WebElement> phoneSection = we.findElements(byPhone);
+				List<WebElement> visitUs = we.findElements(byVisitOurSite);
+				String practiceNameString = (practiceName.size() == 0) ? "NULL" : practiceName.get(0).getText();
+				String fullNameText = (nameAddress.size() == 0) ? "NULL" : nameAddress.get(0).getText();
+				String[] nametab = fullNameText.split("\\r?\\n");
+				String name = nametab[0];
+				String address = "";
+				for (int a = 1; a < nametab.length; a++) {
+					address = address.concat(nametab[a]).concat(", ");
+				}
+				address = address.substring(0,address.lastIndexOf(","));
+				String email = (emailSection.size() == 0) ? ""
+						: emailSection.get(0).getAttribute("href").substring(emailSection.get(0).getAttribute("href").indexOf(":") + 1);
+				String website = (visitUs.size() == 0) ? "" : visitUs.get(0).getAttribute("href");
+				String phone = "";
+				if (phoneSection.size() == 0) {
+					phone = "";
+				} else {
+					for (int b = 0; b < phoneSection.size(); b++) {
+						if (phoneSection.get(b).getText().contains("Tel"))
+							phone = phoneSection.get(b).getText().substring(phoneSection.get(b).getText().indexOf(": ") + 1);
+						else if (phoneSection.get(b).getText().contains("Mob"))
+							phone = phoneSection.get(b).getText().substring(phoneSection.get(b).getText().indexOf(": ") + 1);
+					}
+				}
+
+				refno++;
+				r1.createCell(0).setCellValue(refno);
+				r1.createCell(1).setCellValue(name);
+				r1.createCell(2).setCellValue(profession);
+				r1.createCell(3).setCellValue(practiceNameString);
+				r1.createCell(4).setCellValue(address);
+				r1.createCell(5).setCellValue(phone);
+				r1.createCell(6).setCellValue(email);
+				r1.createCell(7).setCellValue(website);
+				
+				for_end_time = System.currentTimeMillis();
+				for_difference = for_end_time - for_start_time;
+				System.out.println("End of single FOR iteration. rownum value: " + rownum + ". Time taken in ms: " + for_difference);
+			}
+			
 			driver.findElement(byLoadMore).click();
+			startpos += 10;
+			endpos += 10;
 			end_time = System.currentTimeMillis();
 			difference = end_time - start_time;
-			rownum++;
-			System.out.println("now value is: " + rownum + ". Time in ms: " + difference);
+			System.out.println("rownum value: " + rownum + " startpos/endpos: "+ startpos + "/" + endpos + ". WHILE Loop time in ms: " + difference);
 			
 			if ((!driver.findElement(byLoadMore).isDisplayed())) {
 				System.out.println("NameAddress count: " + driver.findElements(byNameAddressText).size());
@@ -88,83 +141,14 @@ public class PhysioScrap {
 				break;
 			}
 		}
-
-		rownum = 0;
-		//List<WebElement> rekordy = driver.findElements(byListItem);
-		int index = 0;
-		int size = driver.findElements(byListItem).size();
-		int refno = 40000;
 		
-		for (int it = size; it > 0; it--) {
-			//WebElement we = rekordy.get(it);
-			
-			String byListItemText = ".//*[@class='articles-item']" + "[" + Integer.toString(it)  + "]";
-			WebElement we = driver.findElement(By.xpath(byListItemText));
-			
-			
-			index++;
-			
-			start_time = System.currentTimeMillis();
-			rownum++;
-			sh.createRow(rownum);
-			Row r1 = sh.getRow(rownum);
-
-			List<WebElement> practiceName = we.findElements(byPracticeName);
-			List<WebElement> nameAddress = we.findElements(byNameAddressText);
-			List<WebElement> emailSection = we.findElements(byEmailUs);
-			List<WebElement> phoneSection = we.findElements(byPhone);
-			List<WebElement> visitUs = we.findElements(byVisitOurSite);
-
-			String practiceNameString = (practiceName.size() == 0) ? "NULL" : practiceName.get(0).getText();
-			
-			String fullNameText = (nameAddress.size() == 0) ? "NULL" : nameAddress.get(0).getText();
-			String[] nametab = fullNameText.split("\\r?\\n");
-			String name = nametab[0];
-			String address = "";
-			for (int a = 1; a < nametab.length; a++) {
-				address = address.concat(nametab[a]).concat(", ");
-			}
-			address = address.substring(0,address.lastIndexOf(","));
-
-			String email = (emailSection.size() == 0) ? ""
-					: emailSection.get(0).getAttribute("href").substring(emailSection.get(0).getAttribute("href").indexOf(":") + 1);
-			String website = (visitUs.size() == 0) ? "" : visitUs.get(0).getAttribute("href");
-			String phone = "";
-			if (phoneSection.size() == 0) {
-				phone = "";
-			} else {
-				for (int b = 0; b < phoneSection.size(); b++) {
-					if (phoneSection.get(b).getText().contains("Tel"))
-						phone = phoneSection.get(b).getText().substring(phoneSection.get(b).getText().indexOf(": ") + 1);
-					else if (phoneSection.get(b).getText().contains("Mob"))
-						phone = phoneSection.get(b).getText().substring(phoneSection.get(b).getText().indexOf(": ") + 1);
-					// phone.concat(phoneSection.get(b).getText()).concat("\r\n");
-				}
-			}
-
-			r1.createCell(0).setCellValue(refno);
-			r1.createCell(1).setCellValue(name);
-			r1.createCell(2).setCellValue(profession);
-			r1.createCell(3).setCellValue(practiceNameString);
-			r1.createCell(4).setCellValue(address);
-			r1.createCell(5).setCellValue(phone);
-			r1.createCell(6).setCellValue(email);
-			r1.createCell(7).setCellValue(website);
-
-			refno++;
-			
-			FileOutputStream out = null;
-			try {
-				out = new FileOutputStream("result.xlsx");
-				wb.write(out);
-				out.close();
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-
-			end_time = System.currentTimeMillis();
-			difference = end_time - start_time;
-			System.out.println("Processing record " + index + " of " + size + ". Time in ms: " + difference);
+		FileOutputStream out = null;
+		try {
+			out = new FileOutputStream("result.xlsx");
+			wb.write(out);
+			out.close();
+		} catch (Exception e1) {
+			e1.printStackTrace();
 		}
 
 		driver.quit();
@@ -172,7 +156,5 @@ public class PhysioScrap {
 		System.out.println("End at: " + dateFormat.format(date));
 
 		return;
-
 	}
-
 }
